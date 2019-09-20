5e2b19943065fe2289da53f0b241e8e3a75b77fe
kangju <2310356723@qq.com>
2019-09-20 18:52:37 +0800

    修正

---
+++ 中酒网/数据扒取/homeval.js
@@ -0,0 +1,51 @@
+var bk = [{
+        "src": "http://img6.zhongjiu.cn/resourceb2b2c/Storage/Shop/1/Products/95390/1_220.png",
+        "name": "42度天佑德青稞酒小黑125ml×4",
+        "price": "￥119.00"
+    },
+    {
+        "src": "http://img6.zhongjiu.cn/resourceb2b2c/Storage/Shop/1/Products/102894/1_220.png",
+        "name": "53°茅台酒股份公司茅台飞天（2015年份）500ml",
+        "price": "￥2499.00"
+    },
+    {
+        "src": "http://img6.zhongjiu.cn/resourceb2b2c/Storage/Shop/1/Products/109626/1_220.png",
+        "name": "42度天佑德2018第17界环湖赛纪念酒150ml*6（礼盒）",
+        "price": "￥198.00"
+    },
+    {
+        "src": "http://img6.zhongjiu.cn/resourceb2b2c/Storage/Shop/1/Products/107497/1_220.png",
+        "name": "53°茅台酒股分公司迎宾酒（2013新版）500ml(6瓶装整箱)",
+        "price": "￥599.00"
+    },
+    {
+        "src": "http://img6.zhongjiu.cn/resourceb2b2c/Storage/Shop/1/Products/80766/1_220.png",
+        "name": "马克斯威酒庄精选美国加利福尼亚州纳帕谷赤霞珠干红2012 750ml",
+        "price": "￥1480.00"
+    },
+    {
+        "src": "http://img6.zhongjiu.cn/resourceb2b2c/Storage/Shop/505/Products/112381/1_220.png",
+        "name": "42度纳曲青稞酒750ml",
+        "price": "￥228.00"
+    },
+    {
+        "src": "http://img6.zhongjiu.cn/resourceb2b2c/Storage/Shop/1/Products/1946/1_220.png",
+        "name": "52°五粮液1995专卖店 500ml",
+        "price": "￥299.00"
+    },
+    {
+        "src": "http://img6.zhongjiu.cn/resourceb2b2c/Storage/Shop/1/Products/81224/1_220.png",
+        "name": "52° 五粮液股份有限公司 五星级 500ml",
+        "price": "￥129.00"
+    },
+    {
+        "src": "http://img6.zhongjiu.cn/resourceb2b2c/Storage/Shop/1/Products/4329/1_220.png",
+        "name": "48度天佑德青稞酒海拔3600 500ml",
+        "price": "￥288.00"
+    },
+    {
+        "src": "http://img6.zhongjiu.cn/resourceb2b2c/Storage/Shop/1/Products/4211/1_220.png",
+        "name": "拉菲珍藏波尔多 750ml（2瓶装）",
+        "price": "￥199.00"
+    }
+]

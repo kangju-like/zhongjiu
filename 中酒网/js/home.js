3b56f9064aa3cf15bf238a8fe7253a69410f0973
kangju <2310356723@qq.com>
2019-09-22 13:09:57 +0800

    修改

---
+++ 中酒网/js/home.js
@@ -0,0 +1,86 @@
+var str = `
+<div class="category">
+                <div class="mc">
+
+                       
+                        <div class="item">
+                            <span>
+                                <h3>
+                                    <i class="icon_bj" style="background:url( http://img6.zhongjiu.cn/resourceb2b2c//Storage/Plat/ImageAd/201609280918494822090.png ) no-repeat;"></i>
+                                    <a href="">葡萄酒</a>
+                                </h3>
+                                <p>
+                                                <a href="" target="_blank">拉菲</a>
+                                                <a href="" target="_blank">张裕</a>
+                                                <a href="" target="_blank" class="active">马克斯威</a>
+                                </p>
+                            </span>
+                            <div class="category-details">
+                                <div class="subitem">
+                                    <dl class="first" style="z-index:99;">
+                                            <dt>品牌</dt>
+                                            <dd class="clearfix">
+                                                <ul>
+                                                                <li><a title="" href="" target="_blank">黄尾袋鼠</a></li>
+                                                                <li><a title="" href="" target="_blank">佩西</a></li>
+                                                                <li><a title="" href="" target="_blank">乐船</a></li>
+                                                                <li><a title="" href="" target="_blank">小龙船</a></li>
+                                                                <li><a title="" href="" target="_blank">特拉托</a></li>
+                                                                <li class="lastChild"><a title="" href="" target="_blank">圣迪斯</a></li>
+                                                                </ul>
+                                                                            <ul>
+                                                                <li><a title="" href="" target="_blank">奔富</a></li>
+                                                                <li><a title="" href="" target="_blank">康迪</a></li>
+
+
+
+                                                </ul>
+                                            </dd>
+                                            <dt>类型</dt>
+                                            <dd class="clearfix">
+                                                <ul>
+                                                                <li><a title="" href="" target="_blank">干红葡萄酒</a></li>
+                                                                <li><a title="" href="" target="_blank">干白葡萄酒</a></li>
+                                                                <li><a title="" href="" target="_blank">气泡/香槟</a></li>
+                                                                <li><a title="" href="" target="_blank">冰酒</a></li>
+                                                                <li><a title="" href="" target="_blank">果酒</a></li>
+                                                                <li class="lastChild"><a title="" href="" target="_blank">甜桃红</a></li>
+                                                                </ul>
+                                                                            <ul>
+                                                                <li><a title="" href="" target="_blank">甜白</a></li>
+
+
+
+                                                </ul>
+                                            </dd>
+                                            <dt>产区</dt>
+                                            <dd class="clearfix">
+                                                <ul>
+                                                                <li><a title="" href="" target="_blank">法国</a></li>
+                                                                <li><a title="" href="" target="_blank">西班牙</a></li>
+                                                                <li><a title="" href="" target="_blank">智利</a></li>
+                                                                <li><a title="" href="" target="_blank">阿根廷</a></li>
+                                                                <li><a title="" href="" target="_blank">澳大利亚</a></li>
+                                                                <li class="lastChild"><a title="" href="" target="_blank">中国</a></li>
+                                                                </ul>
+                                                                            <ul>
+                                                                <li><a title="" href="" target="_blank">美国</a></li>
+
+
+
+                                                </ul>
+                                            </dd>
+
+                                    </dl>
+                                </div>
+                                <div class="cate-right">
+                                    <h5>推荐品牌</h5>
+                                    <ul>
+                                            <li><a href="" title="张裕干红葡萄酒"><img style="" src="http://img6.zhongjiu.cn/resourceb2b2c//Storage/Plat/ImageAd/201703091354318083820.jpg" data-url="http://img6.zhongjiu.cn/resourceb2b2c//Storage/Plat/ImageAd/201703091354318083820.jpg" class="lazyload" alt="张裕干红葡萄酒"></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        
+                </div>
+            </div>`
